4e1225089da04d8adc869f1ca1d72578c8e968b3
devGnode <looterofflux@gmail.com>
2016-01-27 16:25:32 +0100

    Create define.js

---
+++ js/define.js
@@ -0,0 +1,11 @@
+var MDA_DEVICE       = 0x20,
+    VGA_DEVICE_8x8   = 0x01,
+    VGA_DEVICE_8x16  = 0x11,
+    VGA_DEVICE_9x16  = 0x31,
+    CGA_DEVICE_8x8   = 0x02,
+    CGA_DEVICE_8x16  = 0x12,
+    
+    CHAR_8x8  = 0x00,
+    CHAR_8x16 = 0x10,
+    CHAR_9x14 = 0x20,
+    CHAR_9x16 = 0x30;
