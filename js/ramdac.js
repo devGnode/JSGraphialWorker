72fff8b8e5520347d14df98162b43124b6e43b75
devGnode <looterofflux@gmail.com>
2017-02-02 01:25:12 +0100

    Update ramdac.js

--- js/ramdac.js
+++ js/ramdac.js
@@ -1,11 +1,11 @@
 
 var graphicalModeText = function( setting ){
 	var monitor = setting.monitor, 
-		RAMDAC  = setting._gui_,
+		_gui_   = RAMDAC  = setting._gui_,
 		VRAM    = setting.vram,
 		
 		// text buffer
-		CHAR_ROM = setting.charRom, 
+		CHAR_ROM  = setting.charRom || CP_437_8x8, 
 		PALT_ROM = setting.palette,
 		self = {};	
 		
@@ -24,7 +24,28 @@ var graphicalModeText = function( setting ){
 					x+offsetX,
 					y+offsetY,
 					( chr[ i ] === 1 ? clr :
-						( chr[ i ] === 0 && ( fnt || fnt >= 0 ) ? fnt : 0x000000 ) ) );
+					( chr[ i ] === 0 && ( fnt || fnt >= 0 ) ? fnt : _gui_.getPixel( x+offsetX, y+offsetY ) ) ) );
+			}
+		}catch(e){
+			return !1;
+		};
+	return !0;
+	};
+	
+	var putSprite = function( x, y, sprite ){
+		var offsetX = x*monitor.charSize[0],
+			offsetY = y*monitor.charSize[1],
+			i = x = y = 0;
+		try{
+			for(; i< sprite.length; i++,x++ ){
+					
+				x = x === monitor.charSize[0] ? 0 : x;
+				y+= (i%monitor.charSize[0]) === 0 && i != 0 ? 1 : 0;
+				
+				_gui_.setPixel(
+					x+offsetX,
+					y+offsetY,
+					PALT_ROM[  sprite[ i ]&0xff ] );
 			}
 		}catch(e){
 			return !1;
@@ -61,6 +82,23 @@ var graphicalModeText = function( setting ){
 						);
 	};
 
+	self.setCharRom = function( chrRom ){
+		CHAR_ROM = chrRom;
+	};
+	self.setPalette = function( palette ){
+		PALT_ROM = setting.palette = palette;
+	};
+	
+	self.setSprite = function( x, y, sprite ){
+	return putSprite( 
+					x, y,
+				   sprite
+				  );
+	};
+	
+	self._refresh = function( ){
+		RAMDAC.refresh( );
+	};
 	self.refresh = function( vram ){
 		var chr = clr = 0, 
 			len = VRAM.length;
@@ -89,7 +127,3 @@ var graphicalModeText = function( setting ){
 
 	return self;
 };
-
-if( require_pass ){
-	return graphicalModeText;
-}
